0455ce5900b7f010054b09cd432bb3dca940c903
Lethanial Leveille <leveillelethanial@gmail.com>
2026-03-03 14:19:00 -0500

    Add fresh initial schema migration

---
+++ alembic/versions/cc34e0c2349c_baseline_placeholder.py
@@ -0,0 +1,25 @@
+"""baseline placeholder
+
+Revision ID: cc34e0c2349c
+Revises: 
+Create Date: 2026-03-03 13:38:06.750014
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'cc34e0c2349c'
+down_revision: Union[str, Sequence[str], None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+def downgrade() -> None:
+    pass
